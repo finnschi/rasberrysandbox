06399efc44d0d92455497efac35784e446a3e6b4
finnschi <finnschi@gmail.com>
2020-10-25 22:48:18 +0100

    asdasd

--- detector.py
+++ detector.py
@@ -1,7 +1,13 @@
 import RPi.GPIO as GPIO
+import time
 def button_callback(channel):
     print("push detected!")
+    GPIO.output(10, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(10, GPIO.LOW)
 GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(10,GPIO.OUT)
 GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(8,GPIO.BOTH,callback=button_callback)
 message = input("Press enter to quit\n\n") # Run until someone presses enter
