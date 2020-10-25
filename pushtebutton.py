3ef8f20c2e0cd39ed30fdbbf4218b4001bb550a0
finn <finnschi@gmail.com>
2020-10-25 16:44:50 +0100

    yay

--- pushtebutton.py
+++ pushtebutton.py
@@ -8,7 +8,7 @@ def button_callback(channel):
     apobj.notify(
         body='YOUR DOORBELL IS BELLING GO RUN NOW!',
         title='DoorBell',
-        tag="telegram"
+        tag="telegram",
     )
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
--- testpush.py
+++ testpush.py
@@ -2,15 +2,15 @@ import apprise
 
 # Create an Apprise instance
 apobj = apprise.Apprise()
-
+config = apprise.AppriseConfig()
+config.add('/home/pi/.config/apprise.yml')
+apobj.add(config)
 # Add all of the notification services by their server url.
 # A sample email notification:
-apobj.add('tgram://1308991529:AAG6R_lZ5RMEjOirzQt3cQIj00IPTjpRC-I/-433421664')
-
-##https://api.telegram.org/bot1308991529:AAG6R_lZ5RMEjOirzQt3cQIj00IPTjpRC-I/getUpdates
 # Then notify these services any time you desire. The below would
 # notify all of the services loaded into our Apprise object.
 apobj.notify(
     body='YOUR DOORBELL IS BELLING GO RUN NOW!',
     title='DoorBell',
+    tag="telegram",
 )
