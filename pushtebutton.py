3aa64fbb4ec94432749ef73fea7ec9408310706f
finn <finnschi@gmail.com>
2020-10-25 16:02:41 +0100

    add file

--- pushtebutton.py
+++ pushtebutton.py
@@ -1,10 +1,7 @@
 import RPi.GPIO as GPIO
 #import http.client, urllib
-from pushsafer import init, Client
-init("077tGJYCAJfX4NGoqlP4")
 def button_callback(channel):
     print("Dont push me cause I am close to the edge..")
-    Client("").send_message("Message", "Hello", "323", "1", "4", "2", "https://www.pushsafer.com", "Open Pushsafer", "0", "2", "60", "600", "1", "", "", "")
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
