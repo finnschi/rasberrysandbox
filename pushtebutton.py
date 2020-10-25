6fd2effd995653c999f2e627e45c8d342a8d5150
finn <finnschi@gmail.com>
2020-10-25 13:26:14 +0100

    dfdf

--- pushtebutton.py
+++ pushtebutton.py
@@ -4,7 +4,7 @@ def button_callback(channel):
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(8,GPIO.rising,callback=button_callback)
+GPIO.add_event_detect(8,GPIO.RISING,callback=button_callback)
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 GPIO.cleanup() # Clean up
