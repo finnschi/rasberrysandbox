e3db3908578ab065ace6be0510ae034c32c86103
finn <finnschi@gmail.com>
2020-10-25 16:29:47 +0100

    add file

--- testpush.py
+++ testpush.py
@@ -5,7 +5,7 @@ apobj = apprise.Apprise()
 
 # Add all of the notification services by their server url.
 # A sample email notification:
-apobj.add('tgram://1308991529:AAG6R_lZ5RMEjOirzQt3cQIj00IPTjpRC-I/1186998893')
+apobj.add('tgram://1308991529:AAG6R_lZ5RMEjOirzQt3cQIj00IPTjpRC-I/-433421664')
 
 ##https://api.telegram.org/bot1308991529:AAG6R_lZ5RMEjOirzQt3cQIj00IPTjpRC-I/getUpdates
 # Then notify these services any time you desire. The below would
