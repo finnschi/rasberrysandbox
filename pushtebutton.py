8458f3fb9c1cad9c109e92e8446fcda0fcc8a714
finn <finnschi@gmail.com>
2020-10-25 13:06:24 +0100

    added pushtebutton :bug:

--- pushtebutton.py
+++ pushtebutton.py
@@ -3,7 +3,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.IN)
 while True:
         input_value = GPIO.input(8)
-        if input_value == false:
+        if input_value == False:
             print("Dont push me cause I am close to the edge..")
             while input_value == False:
                 input_value = GPIO.input(8)
