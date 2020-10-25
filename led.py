159234fb80d877f5b07badc22f3d0e45006477fa
finnschi <finnschi@gmail.com>
2020-10-25 22:52:42 +0100

    asdasd

---
+++ led.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(10,GPIO.OUT)
+print "LED on"
+GPIO.output(10,GPIO.HIGH)
+time.sleep(1)
+print "LED off"
+GPIO.output(10,GPIO.LOW)
