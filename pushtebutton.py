bffed3b9c7ea07c3592bc80474bfd9f37d34719c
finn <finnschi@gmail.com>
2020-10-25 13:19:08 +0100

    added pushtebutton :bug:

--- pushtebutton.py
+++ pushtebutton.py
@@ -1,10 +1,8 @@
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(8, GPIO.IN)
+GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 while True:
         input_value = GPIO.input(8)
         print(input_value)
-        if input_value == False:
+        if input_value == GPIO.HIGH:
             print("Dont push me cause I am close to the edge..")
-            while input_value == False:
-                input_value = GPIO.input(8)
